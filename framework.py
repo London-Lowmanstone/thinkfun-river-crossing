ed20fc8a36064cef9d4cb29c23796fc1dd0394f9
Jadiker <happylondon4@gmail.com>
2019-06-06 00:36:10 -0500

    Typed framework.py

--- framework.py
+++ framework.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 '''
 Framework for modeling the game River Crossing
 
@@ -5,63 +6,75 @@ See terms.txt for the types of the different terms.
 '''
 
 # pylint: disable=too-few-public-methods, no-else-return, too-many-arguments
+from typing import List, Set, Tuple, Optional, Dict, Iterable
 
 WIDTH = 5
 HEIGHT = 7
 UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3
 
+# see terms.txt for explanations as to what these represent
+Peg = int
+HeldPlank = Optional[int]
+Plank = Tuple[Peg, Peg]
+Move = Tuple[str, Peg]
+# Technically, Direction = Union[Literal[0], Literal[1], Literal[2], Literal[3]]
+# But I didn't want to have to import / install Literal
+Direction = int
+Distance = int
+
 class Board:
     '''
     Represents a River Crossing board
     '''
-    def __init__(self, start, finish, pegs, planks, held_plank=None):
+    def __init__(self, start: Peg, finish: Peg, pegs: Set[Peg], planks: Set[Plank],
+                 held_plank: HeldPlank = None):
         # the starting position of the person
-        # form: peg
         self.person_position = start
+
         # the finish position for the person
         self.finish = finish
+
         # the pegs on the board
-        # form: set of pegs
-        self.pegs = set()
+        self.pegs: Set[Peg] = set()
         self.add_pegs(pegs)
+
         # the planks on the board
-        # form: set of planks
-        self.planks = set()
+        self.planks: Set[Plank] = set()
+
         # pegs that are covered by planks
-        # form: set of pegs
-        self._covered_points = set()
+        self._covered_points: Set[Peg] = set()
         self.add_planks(planks)
+
         # if the person is carrying a plank
-        # form: held plank
         self.held_plank = held_plank
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "<Board: person position: {}, pegs: {}, planks: {}, held_plank: {}>".format(self.person_position, self.pegs, self.planks, self.held_plank) # pylint: disable=line-too-long
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, Board):
             for attribute in ['person_position', 'finish', 'pegs', 'planks', 'held_plank']:
-                if getattr(self, attribute) != getattr(other, attribute):
+                if getattr(self, attribute) != getattr(other, attribute): # type: ignore
                     return False
             return True
         else:
             return False
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.person_position) + hash(tuple(self.planks))
 
-    def __deepcopy__(self, memo):
+    def __deepcopy__(self, memo: Dict[int, object]) -> Board:
         # ignoring `memo` as per https://stackoverflow.com/a/1950593/
         return Board(self.person_position, self.finish, self.pegs.copy(),
                      self.planks.copy(), self.held_plank)
 
-    def get_moves(self):
+    def get_moves(self) -> List[Move]:
         '''Returns a list of all of the available moves'''
         moves = []
         walk_moves = self.get_walk_moves()
 
         if self.can_grab_plank():
-            plank_moves = [("grab", plank_peg) for plank_peg in self.get_grab_planks()]
+            plank_moves: List[Move] = [("grab", plank_peg) for plank_peg in self.get_grab_planks()]
         else:
             plank_moves = [("place", plank_peg) for plank_peg in self.get_place_planks()]
 
@@ -69,7 +82,7 @@ class Board:
         moves.extend(plank_moves)
         return moves
 
-    def get_walk_moves(self):
+    def get_walk_moves(self) -> List[Move]:
         '''Returns a list of all of the available walk moves'''
         ans = []
         for plank in self.planks:
@@ -85,7 +98,7 @@ class Board:
 
         return ans
 
-    def get_grab_planks(self):
+    def get_grab_planks(self) -> List[Peg]:
         '''
         Returns a list of all of the available grab moves.
         Assumption: there is no held plank.
@@ -94,11 +107,13 @@ class Board:
         # any place you can walk to is also a place where you can grab a plank
         return [move for move_type, move in self.get_walk_moves()]
 
-    def get_place_planks(self):
+    def get_place_planks(self) -> List[Peg]:
         '''
         Returns a list of all of the available place moves.
         Assumption: there is a held plank.
         '''
+        assert self.held_plank
+
         ans = []
         for direction in range(4):
             peg = get_peg_in_direction(self.person_position, direction, self.held_plank)
@@ -112,69 +127,72 @@ class Board:
                     ans.append(peg)
         return ans
 
-    def make_move(self, move):
+    def make_move(self, move: Move) -> None:
         '''Makes a move'''
-        move_type, move = move
+        move_type, target = move
         if move_type == "walk":
-            self.person_position = move
+            self.person_position = target
         elif move_type == "grab":
-            self.held_plank = get_peg_dist(self.person_position, move)
-            self.remove_plank(get_plank(self.person_position, move))
+            self.held_plank = get_peg_dist(self.person_position, target)
+            self.remove_plank(get_plank(self.person_position, target))
         elif move_type == "place":
-            self.add_plank(get_plank(self.person_position, move))
+            self.add_plank(get_plank(self.person_position, target))
             self.held_plank = None
         else:
             raise ValueError("Move type {} not recognized".format(move_type))
 
-    def solved(self):
+    def solved(self) -> bool:
         '''Returns boolean whether or not the game has been solved'''
         return self.person_position == self.finish
 
-    def can_grab_plank(self):
+    def can_grab_plank(self) -> bool:
         '''Returns boolean whether or not there is a held plank'''
         return not self.held_plank
 
-    def add_planks(self, planks):
+    def add_planks(self, planks: Iterable[Plank]) -> None:
         '''Adds planks to the set of planks'''
         for plank in planks:
             self.add_plank(plank)
 
-    def add_plank(self, plank):
+    def add_plank(self, plank: Plank) -> None:
         '''Adds a plank to the set of planks; updates the covered points'''
         self.planks.add(plank)
         self._covered_points = self._covered_points.union(get_waypoints(plank))
 
-    def add_pegs(self, pegs):
+    def add_pegs(self, pegs: Iterable[Peg]) -> None:
         '''Adds pegs to the set of pegs'''
         for peg in pegs:
             self.add_peg(peg)
 
-    def add_peg(self, peg):
+    def add_peg(self, peg: Peg) -> None:
         '''Add a peg to the set of pegs'''
         self.pegs.add(peg)
 
-    def remove_plank(self, plank):
+    def remove_plank(self, plank: Plank) -> None:
         '''Removes a plank from the set of planks; updates the covered points'''
         self.planks.remove(plank)
         self._covered_points = self._covered_points.difference(get_waypoints(plank))
 
 
-def get_waypoints(plank):
+def get_waypoints(plank: Plank) -> Set[Peg]:
     '''Gets the points that are not the endpoints of the plank'''
-    ans = set()
+    ans: Set[Peg] = set()
     direction, total_distance = get_peg_direction_dist(*plank)
     if direction is None:
         return ans
+    assert total_distance is not None
     for path_distance in range(1, total_distance):
-        ans.add(get_peg_in_direction(plank[0], direction, how_many=path_distance))
+        waypoint = get_peg_in_direction(plank[0], direction, how_many=path_distance)
+        if waypoint is not None:
+            ans.add(waypoint)
     return ans
 
-def get_plank(peg1, peg2):
+def get_plank(peg1: Peg, peg2: Peg) -> Plank:
     '''Takes two endpoints and returns a plank between those two endpoints'''
-    # a plank is represented by a sorted tuple of the pegs its between
-    return tuple(sorted([peg1, peg2]))
+    # a plank is represented by a sorted tuple of the pegs it's between
+    return (peg1, peg2) if peg1 < peg2 else (peg2, peg1)
 
-def get_peg_dist(peg1, peg2):
+def get_peg_dist(peg1: Peg, peg2: Peg) -> Optional[Distance]:
     '''
     Gets the distance between two pegs.
     Diagonal distance is undefined - this function will return None
@@ -196,11 +214,9 @@ def get_peg_dist(peg1, peg2):
             # they're not in the same column or row; distance is undefined
             return None
 
-def get_peg_direction_dist(peg1, peg2):
+def get_peg_direction_dist(peg1: Peg, peg2: Peg) -> Tuple[Optional[Direction], Optional[Distance]]:
     '''
     Returns the direction from peg1 to peg2 and the distance between them as a tuple
-
-    Form: (direction, distance)
     '''
 
     # upgrade: this code is very similar to get_peg_dist
@@ -212,7 +228,7 @@ def get_peg_direction_dist(peg1, peg2):
     peg2_bigger = max((peg1, peg2)) == peg2
     if peg1_mod == peg2_mod:
         # they're in the same column
-        direction = 2 if peg2_bigger else 0
+        direction: Direction = 2 if peg2_bigger else 0
         return direction, abs_dist // WIDTH
     else:
         # they're not in the same column
@@ -225,7 +241,8 @@ def get_peg_direction_dist(peg1, peg2):
             return None, None
 
 
-def get_peg_in_direction(peg, direction, how_many=1): # pylint: disable=inconsistent-return-statements
+def get_peg_in_direction(peg: Optional[Peg], direction: Direction,
+                         how_many: Distance = 1) -> Optional[Peg]: # pylint: disable=inconsistent-return-statements
     '''Returns the peg the given number of steps away from the given peg in the given direction'''
     if how_many > 1:
         peg = get_peg_in_direction(peg, direction, how_many - 1)
@@ -256,7 +273,11 @@ def get_peg_in_direction(peg, direction, how_many=1): # pylint: disable=inconsis
     else:
         raise ValueError("Unrecognized direction: {}".format(direction))
 
-def count_grab_and_place(moves):
+    # I think this is needed for PEP 8.
+    # See https://github.com/python/mypy/issues/3974#issuecomment-331648044
+    return None
+
+def count_grab_and_place(moves: Iterable[Move]) -> int:
     '''Counts how many grab moves and place moves combined there are. Useful for debugging.'''
     counter = 0
     for move_type, peg in moves: # pylint: disable=unused-variable
@@ -264,6 +285,6 @@ def count_grab_and_place(moves):
             counter += 1
     return counter
 
-def take_out_walk_moves(moves):
+def take_out_walk_moves(moves: Iterable[Move]) -> List[Move]:
     '''Takes out any walk moves from a solution'''
     return [move for move in moves if move[0] != 'walk']
